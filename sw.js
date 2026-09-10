211f741ca8fe9a137c78c057da0121cec4f06c80
ellh19 <ellhastings87@gmail.com>
2026-09-10 01:28:27 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="chequebook-prophecy-v1-22";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.png","./icon-512.png","./title-logo.png"];
+const CACHE="chequebook-prophecy-v1-23";
+const ASSETS=["./","./index.html","./oracle-v23.webmanifest","./oracle-icon-192-v23.png","./oracle-icon-512-v23.png","./title-logo.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))))});
